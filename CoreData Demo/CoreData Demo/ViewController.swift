0a2fe0bf109eebd7abb348216d2fb5b99b054e82
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2023-01-19 15:11:06 +0600

    Add files via upload

---
+++ CoreData Demo/CoreData Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData Demo
+//
+//  Created by Abul Kashem on 19/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
